759477767279fd21f77501a954d3b9965718e1d1
Pedrosilva6109 <pedro.c.silva11@aluno.senai.br>
2025-11-28 09:34:28 -0300

    2

--- src/main/java/br/com/infob/controle_epi/controle_epi/config/Swagger.java
+++ src/main/java/br/com/infob/controle_epi/controle_epi/config/Swagger.java
@@ -1,5 +1,18 @@
 package br.com.infob.controle_epi.controle_epi.config;
 
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Controle de EPI API",
+        version = "1.0",
+        description = "API para o sistema de controle de EPI"
+    )
+)
 public class Swagger {
     
 }
